a13bc22bde0ddd9b4686a101eb586a7b2e065862
simpleframework <cknet@126.com>
2013-11-14 15:59:18 +0800

    Signed-off-by: simpleframework <cknet@126.com>

---
+++ src/main/java/net/simpleframework/module/common/content/IContentService.java
@@ -0,0 +1,68 @@
+package net.simpleframework.module.common.content;
+
+import net.simpleframework.ado.ColumnData;
+import net.simpleframework.ado.FilterItems;
+import net.simpleframework.ado.query.IDataQuery;
+import net.simpleframework.ctx.common.bean.TimePeriod;
+import net.simpleframework.ctx.service.ado.db.IDbBeanService;
+
+/**
+ * Licensed under the Apache License, Version 2.0
+ * 
+ * @author 陈侃(cknet@126.com, 13910090885)
+ *         http://code.google.com/p/simpleframework/
+ *         http://www.simpleframework.net
+ */
+public interface IContentService<T extends AbstractContentBean> extends IDbBeanService<T> {
+
+	/**
+	 * 按指定条件查找
+	 * 
+	 * @param category
+	 *           类目
+	 * @param status
+	 *           状态
+	 * @param timePeriod
+	 *           时间范围
+	 * @param filterItems
+	 *           其它条件
+	 * @param orderColumns
+	 *           排序
+	 * @return
+	 */
+	IDataQuery<T> queryBeans(AbstractCategoryBean category, EContentStatus status,
+			TimePeriod timePeriod, FilterItems filterItems, ColumnData... orderColumns);
+
+	IDataQuery<T> queryBeans(AbstractCategoryBean category, EContentStatus status,
+			TimePeriod timePeriod, FilterItems filterItems);
+
+	/**
+	 * 按类目查找
+	 * 
+	 * @param category
+	 * @return
+	 */
+	IDataQuery<T> queryBeans(AbstractCategoryBean category);
+
+	IDataQuery<T> queryBeans(AbstractCategoryBean category, TimePeriod timePeriod,
+			ColumnData... orderColumns);
+
+	IDataQuery<T> queryBeans(AbstractCategoryBean oCategory, EContentStatus status);
+
+	/**
+	 * 查询推荐内容
+	 * 
+	 * @param category
+	 * @param timePeriod
+	 * @return
+	 */
+	IDataQuery<T> queryRecommendationBeans(AbstractCategoryBean category, TimePeriod timePeriod);
+
+	/**
+	 * 查询我的内容
+	 * 
+	 * @param user
+	 * @return
+	 */
+	IDataQuery<T> queryMyBeans(Object user);
+}
---
+++ src/main/java/net/simpleframework/module/common/content/impl/AbstractContentService.java
@@ -0,0 +1,71 @@
+package net.simpleframework.module.common.content.impl;
+
+import net.simpleframework.ado.ColumnData;
+import net.simpleframework.ado.EFilterRelation;
+import net.simpleframework.ado.FilterItem;
+import net.simpleframework.ado.FilterItems;
+import net.simpleframework.ado.query.IDataQuery;
+import net.simpleframework.ctx.common.bean.TimePeriod;
+import net.simpleframework.ctx.permission.PermissionUser;
+import net.simpleframework.ctx.service.ado.db.AbstractDbBeanService;
+import net.simpleframework.module.common.content.AbstractCategoryBean;
+import net.simpleframework.module.common.content.AbstractContentBean;
+import net.simpleframework.module.common.content.EContentStatus;
+import net.simpleframework.module.common.content.IContentService;
+
+/**
+ * Licensed under the Apache License, Version 2.0
+ * 
+ * @author 陈侃(cknet@126.com, 13910090885)
+ *         http://code.google.com/p/simpleframework/
+ *         http://www.simpleframework.net
+ */
+public abstract class AbstractContentService<T extends AbstractContentBean> extends
+		AbstractDbBeanService<T> implements IContentService<T> {
+
+	protected abstract ColumnData[] getDefaultOrderColumns();
+
+	@Override
+	public IDataQuery<T> queryByParams(final FilterItems params) {
+		return queryByParams(params, getDefaultOrderColumns());
+	}
+
+	@Override
+	public IDataQuery<T> queryBeans(final AbstractCategoryBean category,
+			final EContentStatus status, final TimePeriod timePeriod, final FilterItems filterItems) {
+		return queryBeans(category, status, timePeriod, filterItems, getDefaultOrderColumns());
+	}
+
+	@Override
+	public IDataQuery<T> queryBeans(final AbstractCategoryBean oCategory, final EContentStatus status) {
+		return queryBeans(oCategory, status, null, null);
+	}
+
+	@Override
+	public IDataQuery<T> queryBeans(final AbstractCategoryBean category,
+			final TimePeriod timePeriod, final ColumnData... orderColumns) {
+		return queryBeans(category, EContentStatus.publish, timePeriod, null, orderColumns);
+	}
+
+	@Override
+	public IDataQuery<T> queryBeans(final AbstractCategoryBean category) {
+		return queryBeans(category, (TimePeriod) null, getDefaultOrderColumns());
+	}
+
+	@Override
+	public IDataQuery<T> queryRecommendationBeans(final AbstractCategoryBean category,
+			final TimePeriod timePeriod) {
+		return queryBeans(category, EContentStatus.publish, timePeriod,
+				FilterItems.of(new FilterItem("recommendation", EFilterRelation.gt, 0)));
+	}
+
+	@Override
+	public IDataQuery<T> queryMyBeans(final Object user) {
+		return query(
+				null,
+				null,
+				null,
+				FilterItems.of().addEqualItem("userId",
+						(user instanceof PermissionUser) ? ((PermissionUser) user).getId() : user));
+	}
+}
