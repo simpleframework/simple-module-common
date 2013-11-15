25a581908720673ed2e6ac74c8c641c3e5997c1b
simpleframework <cknet@126.com>
2013-11-15 12:10:45 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/common/bean/IViewsBeanAware.java
+++ src/main/java/net/simpleframework/module/common/bean/IViewsBeanAware.java
@@ -3,8 +3,7 @@ package net.simpleframework.module.common.bean;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 闄堜緝(cknet@126.com, 13910090885)
- *         https://github.com/simpleframework
+ * @author 闄堜緝(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public interface IViewsBeanAware {
