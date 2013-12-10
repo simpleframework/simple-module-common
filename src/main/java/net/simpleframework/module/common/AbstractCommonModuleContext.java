452900a5a449ccd8ce4cdb065cd09ba1338055ca
simpleframework <cknet@126.com>
2013-12-10 10:51:29 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/common/AbstractCommonModuleContext.java
+++ src/main/java/net/simpleframework/module/common/AbstractCommonModuleContext.java
@@ -1,7 +1,7 @@
 package net.simpleframework.module.common;
 
 import net.simpleframework.common.th.NotImplementedException;
-import net.simpleframework.ctx.service.ado.db.AbstractDbModuleContext;
+import net.simpleframework.ctx.AbstractADOModuleContext;
 import net.simpleframework.module.common.content.Attachment;
 import net.simpleframework.module.common.content.IAttachmentService;
 
@@ -11,7 +11,7 @@ import net.simpleframework.module.common.content.IAttachmentService;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public abstract class AbstractCommonModuleContext extends AbstractDbModuleContext implements
+public abstract class AbstractCommonModuleContext extends AbstractADOModuleContext implements
 		ICommonModuleContext {
 
 	@Override
--- src/main/java/net/simpleframework/module/common/ICommonModuleContext.java
+++ src/main/java/net/simpleframework/module/common/ICommonModuleContext.java
@@ -1,6 +1,6 @@
 package net.simpleframework.module.common;
 
-import net.simpleframework.ctx.service.ado.db.IDbModuleContext;
+import net.simpleframework.ctx.IADOModuleContext;
 import net.simpleframework.module.common.content.Attachment;
 import net.simpleframework.module.common.content.IAttachmentService;
 
@@ -10,7 +10,7 @@ import net.simpleframework.module.common.content.IAttachmentService;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public interface ICommonModuleContext extends IDbModuleContext {
+public interface ICommonModuleContext extends IADOModuleContext {
 
 	/**
 	 * 获取附件管理器
