ecc1a9eb8f64aa28c57a573d6055b305f1e9893c
simpleframework <cknet@126.com>
2013-12-13 13:15:04 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/common/content/ContentException.java
+++ src/main/java/net/simpleframework/module/common/content/ContentException.java
@@ -1,6 +1,6 @@
 package net.simpleframework.module.common.content;
 
-import net.simpleframework.common.th.RuntimeExceptionEx;
+import net.simpleframework.ctx.ModuleException;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -8,7 +8,7 @@ import net.simpleframework.common.th.RuntimeExceptionEx;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public class ContentException extends RuntimeExceptionEx {
+public class ContentException extends ModuleException {
 
 	public ContentException(final String msg, final Throwable cause) {
 		super(msg, cause);
@@ -18,8 +18,8 @@ public class ContentException extends RuntimeExceptionEx {
 		return _of(ContentException.class, msg);
 	}
 
-	public static RuntimeException of(final Throwable throwable) {
-		return _of(ContentException.class, null, throwable);
+	public static ContentException of(final Throwable throwable) {
+		return (ContentException) _of(ContentException.class, null, throwable);
 	}
 
 	private static final long serialVersionUID = -3871370008470558392L;
