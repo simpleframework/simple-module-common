c47cb261f860d122d25965c457520de93e862276
simpleframework <cknet@126.com>
2013-12-17 11:11:51 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/common/ICommonModuleContext.java
+++ src/main/java/net/simpleframework/module/common/ICommonModuleContext.java
@@ -1,6 +1,6 @@
 package net.simpleframework.module.common;
 
-import net.simpleframework.ctx.IADOModuleContext;
+import net.simpleframework.ctx.IModuleContext;
 import net.simpleframework.module.common.content.Attachment;
 import net.simpleframework.module.common.content.IAttachmentService;
 
@@ -10,7 +10,7 @@ import net.simpleframework.module.common.content.IAttachmentService;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public interface ICommonModuleContext extends IADOModuleContext {
+public interface ICommonModuleContext extends IModuleContext {
 
 	/**
 	 * 获取附件管理器
