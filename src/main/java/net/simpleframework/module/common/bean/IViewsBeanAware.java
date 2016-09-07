7622df683afebfbaad07dc4942574e6a139f9ae2
CHENK <CHENK@CHENK-PC>
2016-09-07 14:19:17 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/common/bean/IViewsBeanAware.java
+++ src/main/java/net/simpleframework/module/common/bean/IViewsBeanAware.java
@@ -5,7 +5,8 @@ import java.io.Serializable;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 闄堜緝(cknet@126.com, 13910090885) https://github.com/simpleframework
+ * @author 闄堜緝(cknet@126.com, 13910090885)
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public interface IViewsBeanAware extends Serializable {
